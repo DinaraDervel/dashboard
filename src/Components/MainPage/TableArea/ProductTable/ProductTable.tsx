04bcafa838122c0518d10b7560bd8a3d3c1d2eea
DinaraDervel <dinaradervel@gmail.com>
2024-09-23 21:00:20 -0600

    add horizontal scroll to table

--- src/Components/MainPage/TableArea/ProductTable/ProductTable.tsx
+++ src/Components/MainPage/TableArea/ProductTable/ProductTable.tsx
@@ -34,94 +34,103 @@ const ProductTable = observer(() => {
   }
 
   return (
-    <Paper sx={{ width: "100%", overflow: "auto", borderRadius: 5, mt: 3 }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                Баркод
-                <ArrowDropDownIcon
-                  color="primary"
-                  onClick={() => sort("barcode")}
-                />
-              </TableCell>
-              <TableCell>
-                Предмет
-                <ArrowDropDownIcon onClick={() => sort("item")} />
-              </TableCell>
-              <TableCell>
-                Артикул поставщика
-                <ArrowDropDownIcon onClick={() => sort("companyArticle")} />
-              </TableCell>
-              <TableCell>
-                Размер
-                <ArrowDropDownIcon onClick={() => sort("size")} />
-              </TableCell>
-              <TableCell>
-                Доступно к заказу
-                <ArrowDropDownIcon onClick={() => sort("available")} />
-              </TableCell>
-              <TableCell>
-                Товары в пути
-                <ArrowDropDownIcon onClick={() => sort("onTheWay")} />
-              </TableCell>
-              <TableCell>
-                Итого кол-во товаров
-                <ArrowDropDownIcon onClick={() => sort("total")} />
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {productStore.products.map((product: Product) => (
-              <TableRow key={product.barcode}>
-                <TableCell>{product.barcode}</TableCell>
-                <EditableTableCell
-                  product={product}
-                  field="item"
-                  onSave={productStore.update}
-                />
-                <EditableTableCell
-                  product={product}
-                  field="companyArticle"
-                  onSave={productStore.update}
-                />
-                <EditableTableCell
-                  product={product}
-                  field="size"
-                  onSave={productStore.update}
-                  isNumeric
-                />
-                <EditableTableCell
-                  product={product}
-                  field="available"
-                  onSave={productStore.update}
-                  isNumeric
-                />
-                <EditableTableCell
-                  product={product}
-                  field="onTheWay"
-                  onSave={productStore.update}
-                  isNumeric
-                />
-                <EditableTableCell
-                  product={product}
-                  field="total"
-                  onSave={productStore.update}
-                  isNumeric
-                />
-              </TableRow>
-            ))}
-          </TableBody>
+    <TableContainer
+      component={Paper}
+      sx={{
+        maxHeight: 440,
+        width: "80%",
+        overflowX: "auto",
+        overflowY: "auto",
+        borderRadius: 5,
+        mt: 3,
+        whiteSpace: "nowrap",
+      }}
+    >
+      <Table stickyHeader>
+        <TableHead>
           <TableRow>
-            <TableCell colSpan={4}>Итого:</TableCell>
-            <TableCell>{total("available")}</TableCell>
-            <TableCell>{total("onTheWay")}</TableCell>
-            <TableCell>{total("total")}</TableCell>
+            <TableCell>
+              Баркод
+              <ArrowDropDownIcon
+                color="primary"
+                onClick={() => sort("barcode")}
+              />
+            </TableCell>
+            <TableCell>
+              Предмет
+              <ArrowDropDownIcon onClick={() => sort("item")} />
+            </TableCell>
+            <TableCell>
+              Артикул поставщика
+              <ArrowDropDownIcon onClick={() => sort("companyArticle")} />
+            </TableCell>
+            <TableCell>
+              Размер
+              <ArrowDropDownIcon onClick={() => sort("size")} />
+            </TableCell>
+            <TableCell>
+              Доступно к заказу
+              <ArrowDropDownIcon onClick={() => sort("available")} />
+            </TableCell>
+            <TableCell>
+              Товары в пути
+              <ArrowDropDownIcon onClick={() => sort("onTheWay")} />
+            </TableCell>
+            <TableCell>
+              Итого кол-во товаров
+              <ArrowDropDownIcon onClick={() => sort("total")} />
+            </TableCell>
           </TableRow>
-        </Table>
-      </TableContainer>
-    </Paper>
+        </TableHead>
+        <TableBody>
+          {productStore.products.map((product: Product) => (
+            <TableRow key={product.barcode}>
+              <TableCell>{product.barcode}</TableCell>
+              <EditableTableCell
+                product={product}
+                field="item"
+                onSave={productStore.update}
+              />
+              <EditableTableCell
+                product={product}
+                field="companyArticle"
+                onSave={productStore.update}
+              />
+              <EditableTableCell
+                product={product}
+                field="size"
+                onSave={productStore.update}
+                isNumeric
+              />
+              <EditableTableCell
+                product={product}
+                field="available"
+                onSave={productStore.update}
+                isNumeric
+              />
+              <EditableTableCell
+                product={product}
+                field="onTheWay"
+                onSave={productStore.update}
+                isNumeric
+              />
+              <EditableTableCell
+                product={product}
+                field="total"
+                onSave={productStore.update}
+                isNumeric
+              />
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableRow>
+          <TableCell colSpan={4}>Итого:</TableCell>
+          <TableCell>{total("available")}</TableCell>
+          <TableCell>{total("onTheWay")}</TableCell>
+          <TableCell>{total("total")}</TableCell>
+        </TableRow>
+      </Table>
+    </TableContainer>
   );
 });
 
